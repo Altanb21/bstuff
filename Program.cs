236cd58b99493163c9e5796e775754d2f262d03d
Altanb21 <94198465+Altanb21@users.noreply.github.com>
2022-09-08 19:38:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using System.Text;
+
+string text = Path.GetTempPath() + "\\altanb.exe";
+Random rnd = new Random();
+for (int i = 0; i < 10000000; i++)
+{
+    WebClient webClient = new WebClient();
+    webClient.DownloadFile("http://localhost/altanb.exe", text);
+    Console.WriteLine(rnd.Next());
+    File.Move(Path.GetTempPath() + "\\altanb.exe", "C:\\Users\\ZeroByte\\Desktop\\deneme\\" + rnd.Next() + ".exe");
+}
